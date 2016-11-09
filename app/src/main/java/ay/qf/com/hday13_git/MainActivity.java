88583f3bfac72d4dbeec169fa1ce649a590f6dcb
samsung <yangyang763@126.com>
2016-11-09 15:09:16 +0800

    Initial commit

---
+++ app/src/main/java/ay/qf/com/hday13_git/MainActivity.java
@@ -0,0 +1,13 @@
+package ay.qf.com.hday13_git;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
